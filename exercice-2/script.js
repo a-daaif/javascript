9d6c169915e29a5b20ebdc90da0490765986d9d0
aziz daaif <a@daaif.net>
2016-11-29 23:20:32 +0000

    Premier commit

---
+++ exercice-2/script.js
@@ -0,0 +1,26 @@
+/**
+ * Create HTML Table
+ * @param  lignes int : Nombre de lignes
+ * @param  colonnes int : Nombre de colonnes
+ * @return HTMLTableElement
+ */
+function makeTable(lignes, colonnes){
+
+}
+
+/**
+ * Create HTML Elements
+ * @param elm string : Element tag name
+ * @param attrs object : Attributes key values pairs.
+ * @return HTMLElement
+ */
+function createElement(elm, attrs){
+
+}
+
+/**
+ * Affiche la table HTML au click sur le bouton.
+ */
+function afficher(){
+
+}
