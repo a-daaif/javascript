f4b77f624a46b00ad1af108f7013adcd14c7c211
aziz daaif <a@daaif.net>
2016-12-01 11:47:06 +0000

    Ajout de l'exercice mail

---
+++ exercice-js-mail/js/mail.js
@@ -0,0 +1,35 @@
+// Ajouter l'événement click au bouton
+
+
+/**
+ * Ajoute une ligne à la fin du tbody de la table
+ */
+function ajouterLigne(){
+
+}
+
+/**
+ * Crée un élément tr (ligne du tableau)
+ * @param  object string : L'objet du message à extraire de la zone de texte
+ * @param  message string : le message à extraire de la zone de texte
+ * @param etat string : Normal ou Urgent
+ * @return HTMLTableRowElement : Elément de type tr prêt à être inséré dans le tableau.
+ */
+function creerLigne(object, message, etat){
+
+}
+
+/**
+ * Supprime une ligne
+ * @param tr HTMLTableRowElement : La ligne qu'on veut supprimer
+ */
+function supprimerLigne(tr){
+
+}
+
+/**
+ * Supprime toutes les lignes sélectionnées
+ */
+function supprimerLesLignesSelectionnees(){
+
+}
